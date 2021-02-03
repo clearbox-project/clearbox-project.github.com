f64aee8c14a6d0b831debefdab64590442453405
Conan <conanchou1412@gmail.com>
2021-02-03 14:33:27 -0500

    init site

---
+++ js/about.3cd84cef.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),s={class:"about"},c=Object(e["j"])("h1",null,"This is an about page",-1);function o(n,t){return Object(e["s"])(),Object(e["f"])("div",s,[c])}const u={};u.render=o;t["default"]=u}}]);
+//# sourceMappingURL=about.3cd84cef.js.map
